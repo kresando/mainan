6124248b6a29938c063159fff66c4194702f1aaa
kresando <79419057+kresando@users.noreply.github.com>
2025-04-30 23:11:42 +0800

    Fix Middleware permission

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use App\Http\Middleware\TrustProxies;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,6 +12,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->prepend(TrustProxies::class);
+
         $middleware->alias([
             'track.views' => \App\Http\Middleware\TrackPostViews::class,
         ]);
