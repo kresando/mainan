4b869c5076c959a87d37e265a07243c377dd6c38
kresando <79419057+kresando@users.noreply.github.com>
2025-05-02 03:43:13 +0800

    Fix Thumbnail

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -28,9 +28,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         // Force HTTPS in production
-        if ($this->app->environment('production')) {
-            URL::forceScheme('https');
-        }
+        // if ($this->app->environment('production')) {
+        //     URL::forceScheme('https');
+        // }
 
         // Enable strict mode only in local environment AND not during console commands
         if ($this->app->environment('local') && !$this->app->runningInConsole()) {
