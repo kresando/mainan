e1daf9c09426cdae40d61b9fb3eb22d856a5f949
kresando <79419057+kresando@users.noreply.github.com>
2025-05-01 18:27:47 +0800

    Custom admin path

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -25,7 +25,7 @@ class AdminPanelProvider extends PanelProvider
         return $panel
             ->default()
             ->id('admin')
-            ->path('admin')
+            ->path(env('FILAMENT_PATH', 'admin'))
             ->login()
             ->colors([
                 'primary' => Color::Amber,
